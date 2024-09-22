cf3d57a7423d7da361dd51e8bc51634799e7837f
Ubaidullah28 <bscs22087@itu.edu.pk>
2024-09-22 12:03:24 +0500

    home data in js

--- object.js
+++ object.js
@@ -1,3 +1,10 @@
 const portfolioData = {
-   
+    name: "M Ubaidullah Ishtiaque",
+    title: "Web Developer",
+    description: "Skilled in HTML, CSS, and frontend development with some experience in backend technologies, including the .NET Framework. Proficient in C++, C#, C, and Python, with hands-on knowledge of React for building dynamic user interfaces.",
+    socialLinks: {
+        facebook: "https://m.facebook.com/ubaidullah322/",
+        linkedin: "https://www.linkedin.com/in/muhammad-ubaidullah-7723a1263/",
+        github: "https://github.com/Ubaidullah28"
+    },
 };
